0a7dcae68c0263321b2e1067cd45021fdcfed518
Jean Koerich <jean.koerich@unidavi.edu.br>
2025-02-28 23:19:08 -0300

    ajuste

--- resultado.php
+++ resultado.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
-</head> 
+</head>
 <body>
     <h2>Dados Recebidos</h2>
 
